ac17bed05ea051c10a02e59263f0eedda6a23ca6
sion <sion@genohm.com>
2019-12-04 13:18:29 +0100

    Added day 4

--- src/days/Day.java
+++ src/days/Day.java
@@ -12,8 +12,8 @@ public abstract class Day {
 	public static final Day[] DAYS = new Day[] {
 			new Day1(),
 			new Day2(),
-			new Day3()/*,
-			new Day4(),
+			new Day3(),
+			new Day4()/*,
 			new Day5(),
 			new Day6(),
 			new Day7(),
