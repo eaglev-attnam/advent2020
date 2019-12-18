70e9caa08a60a7376800f8eaad167d1940c2fdd6
sion <sion@genohm.com>
2019-12-18 18:46:28 +0100

    Day 18 Part 2 Ugly version

--- src/days/Day18.java
+++ src/days/Day18.java
@@ -11,6 +11,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -97,7 +98,7 @@ public class Day18 extends Day {
 			}
 		}
 		if(done.size() < 10) {
-			System.out.println("Resolved level " + key.length() + " round " + key + ".");
+			// System.out.println("Resolved level " + key.length() + " round " + key + ".");
 		}
 		calculated.put(key, best);
 		return best;
@@ -183,6 +184,112 @@ public class Day18 extends Day {
 
 	@Override
 	protected Object part2(List<String> input) {
-		return null;
+		LC_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+		char[][] map = new char[input.get(0).length()][input.size()];
+		Map<Character, Coordinate> items = new HashMap<>();
+		for(int y = 0; y < input.size(); y++) {
+			String row = input.get(y);
+			for(int x = 0; x < input.get(0).length(); x++) {
+				char c = row.charAt(x);
+				map[x][y] = c;
+				if(c != '#' && c != '.') {
+					items.put(c, new Coordinate(x, y));
+				}
+			}
+		}
+
+		// @ # %
+		// # # #
+		// $ # &
+
+		map[39][39] = '@';
+		map[39][40] = '#';
+		map[39][41] = '$';
+		map[40][39] = '#';
+		map[40][40] = '#';
+		map[40][41] = '#';
+		map[41][39] = '%';
+		map[41][40] = '#';
+		map[41][41] = '&';
+
+		items.put('@', new Coordinate(39, 39));
+		items.put('$', new Coordinate(39, 41));
+		items.put('%', new Coordinate(41, 39));
+		items.put('&', new Coordinate(41, 41));
+
+		Map<Character, Map<Character, Integer>> distances = new HashMap<>();
+		Map<Character, Map<Character, List<Character>>> neededKeys = new HashMap<>();
+
+		for(Character c : (LC_LETTERS + "@$%&").toCharArray()) {
+			distances.put(c, new HashMap<>());
+			neededKeys.put(c, new HashMap<>());
+		}
+		Set<Character> starts = new HashSet<>();
+		starts.add('@');
+		starts.add('$');
+		starts.add('%');
+		starts.add('&');
+
+		for(int i = 0; i < LC_LETTERS.length(); i++) {
+			char key = LC_LETTERS.charAt(i);
+			for(Character start : starts) {
+				Optional<Pair<Integer, List<Character>>> distAndDoors = getDistAndDoorsOpt(map, items.get(start), items.get(key));
+				if(distAndDoors.isPresent()) {
+					distances.get(start).put(key, distAndDoors.get().getKey());
+					neededKeys.get(start).put(key, distAndDoors.get().getValue());
+				}
+			}
+			for(int j = i + 1; j < LC_LETTERS.length(); j++) {
+				char key2 = LC_LETTERS.charAt(j);
+				Optional<Pair<Integer, List<Character>>> distAndDoors2 = getDistAndDoorsOpt(map, items.get(key), items.get(key2));
+				if(distAndDoors2.isPresent()) {
+					distances.get(key).put(key2, distAndDoors2.get().getKey());
+					neededKeys.get(key).put(key2, distAndDoors2.get().getValue());
+					distances.get(key2).put(key, distAndDoors2.get().getKey());
+					neededKeys.get(key2).put(key, distAndDoors2.get().getValue());
+				}
+			}
+		}
+
+		return shortestDist(distances, neededKeys, new Character[]{'@', '$', '%', '&'}, new HashSet<>(), new HashMap<>());
+	}
+
+	private Optional<Pair<Integer, List<Character>>> getDistAndDoorsOpt(char[][] map, Coordinate start, Coordinate end) {
+		if(((start.getX() < 40) != (end.getX() < 40)) || ((start.getY()) < 40 != (end.getY() < 40))) {
+			return Optional.empty();
+		}
+		return Optional.of(getDistAndDoors(map, start, end));
+	}
+
+	private int shortestDist(Map<Character, Map<Character, Integer>> distances, Map<Character, Map<Character, List<Character>>> blockingDoors,
+							 Character[] current, Set<Character> done, Map<String, Integer> calculated) {
+		String key = done.stream().sorted().map(a -> a + "").collect(Collectors.joining()) + ":" + Arrays.toString(current);
+		if(done.size() == LC_LETTERS.length()) {
+			return 0;
+		} else if (calculated.containsKey(key)){
+			return calculated.get(key);
+		}
+		int best = Integer.MAX_VALUE;
+		for(Character c : LC_LETTERS.toCharArray()) {
+			for(int i = 0; i < current.length; i++) {
+				char pos = current[i];
+				if (distances.get(pos).containsKey(c) && pos != c && !done.contains(c) && done.containsAll(blockingDoors.get(pos).get(c))) {
+					int dist = distances.get(pos).get(c);
+					Set<Character> newDone = new HashSet<>(done);
+					newDone.add(c);
+					Character[] newCurrent = Arrays.copyOf(current, 4);
+					newCurrent[i] = c;
+					dist += shortestDist(distances, blockingDoors, newCurrent, newDone, calculated);
+					if (dist < best) {
+						best = dist;
+					}
+				}
+			}
+		}
+		if(done.size() < 10) {
+			// System.out.println("Resolved level " + key.length() + " round " + key + ".");
+		}
+		calculated.put(key, best);
+		return best;
 	}
 }
